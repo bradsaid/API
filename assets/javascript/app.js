5a7ef3edbf656589245bd986390187bd8754b471
Brad Said <bradsaid@gmail.com>
2018-06-06 20:03:46 -0500

    Why is it appending mutilpe gifs (one for each current gif?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,20 +14,38 @@ $( document ).ready(function(){
 
     $("#find-gif").on("click", function(event) {
         event.preventDefault();
-        var gif = $("#gif-input").val().trim();
+        let gif = $("#gif-input").val().trim();
         topics.push(gif);
         gifButtons();
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=Z4vKSU5AoUMXiTjlS9jE7xDrLISmaqO3";
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=Z4vKSU5AoUMXiTjlS9jE7xDrLISmaqO3";
          $.ajax({
          url: queryURL,
          method: "GET"
-         }).then(function(response) {
-      console.log(response);
-      console.log(response.data[0]);
-      url = response.data[0].images.fixed_width.url
-      $("#gif-view").append("<img src=" + url + ">");
+        }).then(function(response) {
+            console.log(response);
+            console.log(response.data[0]);
+            url = response.data[0].images.fixed_width.url;
+            $("#gif-view").append("<img src=" + url + ">");
     });
 
+    function displayGif() {
+        let gifButton = $(this).attr("data-name");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifButton + "&api_key=Z4vKSU5AoUMXiTjlS9jE7xDrLISmaqO3";
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+          console.log(response);
+          url = response.data[0].images.fixed_width.url;
+            $("#gif-view").append("<img src=" + url + ">");
+        });
+
+      }
+
+    $(document).on("click", ".gif", displayGif);
+    gifButtons();
     });
 
 });
+
+
